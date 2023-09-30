7bfc77fd16a13832634e0b3df0e9cf3922ba4dd9
Melevyse <gmod2012@yandex.ru>
2023-10-01 00:36:49 +0300

    Добавьте файлы проекта.

---
+++ MindBoxLibraryProject/LibraryGeometry.cs
@@ -0,0 +1,24 @@
+﻿namespace Libraries
+{
+    public static class LibraryGeometry
+    {
+        public static double CalculateCircleArea(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static double CalculateTriangleArea(double a, double b, double c)
+        {
+            double s = (a + b + c) / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            return area;
+        }
+
+        public static bool CheckingTriangleRight(double a, double b, double c)
+        {
+            if ((Math.Pow(a, 2) == Math.Pow(b, 2) + Math.Pow(c, 2)) || (Math.Pow(b, 2) == Math.Pow(a, 2) + Math.Pow(c, 2)) || (Math.Pow(c, 2) == Math.Pow(a, 2) + Math.Pow(b, 2)))
+                return true;
+            else return false;
+        }
+    }
+}
